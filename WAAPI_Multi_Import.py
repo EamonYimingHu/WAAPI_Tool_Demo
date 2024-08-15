3c7b514a1a6b4c3038b0bf8d14e12c5fc9cfbd93
EamonYimingHu <166332272+EamonYimingHu@users.noreply.github.com>
2024-08-16 00:15:09 +0100

    Update WAAPI_Multi_Import.py

--- WAAPI_Multi_Import.py
+++ WAAPI_Multi_Import.py
@@ -4,19 +4,21 @@ import os
 # Function to import audio files using WAAPI
 def waapi_import_file(client: WaapiClient, work_unit_name: str, import_actor_mixer: str, container_type: str,
                       container_name: str, sound_type: str, audio_files: list):
+    #创建空列表imports，用于存储批量导入选择的wav文件
     imports = []
     for i, audio_file in enumerate(audio_files, start=1):
         object_path = f"\\Actor-Mixer Hierarchy\\<WorkUnit>{work_unit_name}\\<Actor-Mixer>{import_actor_mixer}\\<{container_type}>{container_name}\\<Sound>{sound_type}_{container_name}_{i:02}"
         audio_file_path = os.path.abspath(audio_file)
-        import_data = {
+      #遍历到的每个wav文件，创建字典用于json格式传入
+        _import = {
             'audioFile': audio_file_path,
             'objectPath': object_path,
 
         }
-        imports.append(import_data)
+        imports.append(_import)
 
     args = {
-        "importOperation": "createNew",
+        "importOperation": "useExisting",
         "default": {
             "importLanguage": sound_type
         },
